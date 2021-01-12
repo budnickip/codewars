b4e0ac1af99e37bd751bb5677df493a54e087c68
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-01-12 15:55:22 +0100

    change in lvl1 ->2 array to enum

--- typescript/lvl2/1/app.ts
+++ typescript/lvl2/1/app.ts
@@ -1,9 +1,8 @@
 enum Alphabet {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z}
-
+const abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 function position(alphabet:string):string {
-  let result: number;
-  result = Alphabet[alphabet]+1;
- return "Position of alphabet: " + result;
+  return `Position of alphabet: ${abc.indexOf(alphabet)+1}`;
+ //return "Position of alphabet: " + Alphabet.alphabet
 }
 console.log(position("a")," Position of alphabet: 1")
 console.log(position("z"),"Position of alphabet: 26")
