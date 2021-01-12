9eb90f5c385d3a693fd8d19ea16e74e4e1aa61be
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-01-12 16:08:13 +0100

    +1 lvl2

--- typescript/lvl2/1/app.js
+++ typescript/lvl2/1/app.js
@@ -1,37 +1,6 @@
-var Alphabet;
-(function (Alphabet) {
-    Alphabet[Alphabet["a"] = 0] = "a";
-    Alphabet[Alphabet["b"] = 1] = "b";
-    Alphabet[Alphabet["c"] = 2] = "c";
-    Alphabet[Alphabet["d"] = 3] = "d";
-    Alphabet[Alphabet["e"] = 4] = "e";
-    Alphabet[Alphabet["f"] = 5] = "f";
-    Alphabet[Alphabet["g"] = 6] = "g";
-    Alphabet[Alphabet["h"] = 7] = "h";
-    Alphabet[Alphabet["i"] = 8] = "i";
-    Alphabet[Alphabet["j"] = 9] = "j";
-    Alphabet[Alphabet["k"] = 10] = "k";
-    Alphabet[Alphabet["l"] = 11] = "l";
-    Alphabet[Alphabet["m"] = 12] = "m";
-    Alphabet[Alphabet["n"] = 13] = "n";
-    Alphabet[Alphabet["o"] = 14] = "o";
-    Alphabet[Alphabet["p"] = 15] = "p";
-    Alphabet[Alphabet["q"] = 16] = "q";
-    Alphabet[Alphabet["r"] = 17] = "r";
-    Alphabet[Alphabet["s"] = 18] = "s";
-    Alphabet[Alphabet["t"] = 19] = "t";
-    Alphabet[Alphabet["u"] = 20] = "u";
-    Alphabet[Alphabet["v"] = 21] = "v";
-    Alphabet[Alphabet["w"] = 22] = "w";
-    Alphabet[Alphabet["x"] = 23] = "x";
-    Alphabet[Alphabet["y"] = 24] = "y";
-    Alphabet[Alphabet["z"] = 25] = "z";
-})(Alphabet || (Alphabet = {}));
-var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-function position(alphabet) {
-    return "Position of alphabet: " + (abc.indexOf(alphabet) + 1);
-    //return "Position of alphabet: " + Alphabet.alphabet
+function solution(str, ending) {
+    return ending ? str ? str.endsWith(ending) : false : true;
 }
-console.log(position("a"), " Position of alphabet: 1");
-console.log(position("z"), "Position of alphabet: 26");
-console.log(position("e"), "Position of alphabet: 5");
+console.log(solution('abcde', 'cde'), " true");
+console.log(solution('abcde', 'abc'), " false");
+console.log(solution('abc', ''), " true");
--- typescript/lvl2/1/app.ts
+++ typescript/lvl2/1/app.ts
@@ -1,9 +1,10 @@
-enum Alphabet {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z}
-const abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-function position(alphabet:string):string {
-  return `Position of alphabet: ${abc.indexOf(alphabet)+1}`;
- //return "Position of alphabet: " + Alphabet.alphabet
+interface String {    
+  endsWith(searchString: string, endPosition?: number): boolean;
 }
-console.log(position("a")," Position of alphabet: 1")
-console.log(position("z"),"Position of alphabet: 26")
-console.log(position("e"),"Position of alphabet: 5")
+
+function solution(str: string, ending: string): boolean {
+  return ending ? str ? str.endsWith(ending) : false : true
+}
+console.log(solution('abcde', 'cde'), " true")
+console.log(solution('abcde', 'abc'), " false")
+console.log(solution('abc', ''), " true")
