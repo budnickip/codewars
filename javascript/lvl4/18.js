c82473744809bedf7d87126906721dc7ce417b1e
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-02-23 18:54:20 +0100

    +1 lvl4

---
+++ javascript/lvl4/18.js
@@ -0,0 +1,31 @@
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+*/
+
+var moveZeros = function (arr) {
+    // TODO: Program me
+    let newArr = []
+    let zeroArray = []
+    arr.forEach(item => {
+        if(item === 0){
+            zeroArray.push(item)
+        }else{
+            newArr.push(item)
+        }
+    })
+    return newArr.concat(zeroArray)
+  }
+
+
+console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]), [ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
+
+/*
+Lepsze rozwiązanie:
+var moveZeros = function (arr) {
+  return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
+}
+
+*/
